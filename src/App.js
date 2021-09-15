dbaa5dab0628a2baaee874e8ca4f7225550ad353
Kacper Bąk <kacper.bak@sonderfin.com>
2021-09-16 00:02:44 +0200

    feat: some changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
+        <h1>55 Center</h1>
+        <p class="line-1 anim-typewriter">Brings with it the vibrations of curiosity</p>
       </header>
     </div>
   );
